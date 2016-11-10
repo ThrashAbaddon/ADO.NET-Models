43c09acae91c9a2ecb377a7e6b7448261b80895d
Daniel <straca369@gmail.com>
2016-11-10 19:53:15 +0100

    small refractor, added comments

--- ConsoleDB/CreateDataColumnForDataTable.cs
+++ ConsoleDB/CreateDataColumnForDataTable.cs
@@ -11,10 +11,10 @@ namespace ConsoleDB
     {
         static void Main(string[] args)
         {
-            
+
         }
 
-        static void KreiraStupceKojeDodaUTablicu()
+        static void AddsColumnsToTheTable()
         {
             DataTable dt = new DataTable();
 
@@ -67,35 +67,38 @@ namespace ConsoleDB
         }
 
 
-        static void DodajeTablicuSetu()
+        static void AddsTableToDataSet()
         {
             DataSet ds = new DataSet();
 
             // Add a DataTable named Table-1 directly
-            DataTable dt1 = ds.Tables.Add("Table-1");
+            DataTable dt1 = ds.Tables.Add("Tablica-1");
             // ... Configure the DataTable -- add some columns, etc.
 
-            // Add a DataTable named Table-2 by creating the table
+            // Add a DataTable named Tablica-2 by creating the table
             // and adding it to the DataSet
-            DataTable dt2 = new DataTable("Table-2");
-            // ... Configure the DataTable -- add some columns, etc.
-            ds.Tables.Add(dt2);
+            DataTable dt2 = new DataTable("Tablica-2");
+            DataColumn column1 = dt2.Columns.Add();
 
-            // Add multiple DataTables to the DataSet
-            DataTable dt3 = new DataTable("Table-3");
-            DataTable dt4 = new DataTable("Table-4");
-            // ... Configure the DataTable -- add some columns, etc.
+            // Configure the column, set integer with a default = 0 that
+            // does not allow nulls
+            column1.ColumnName = "Id";
+            column1.DataType = typeof(int);
+            column1.DefaultValue = 0;
+            column1.Unique = true;
+            column1.AllowDBNull = false;
+
+            // Add multiple empty DataTables to the DataSet
+            DataTable dt3 = new DataTable("Tablica-3");
+            DataTable dt4 = new DataTable("Tablica-4");
+            // Configure the DataTable -- add some columns, etc., if want
+            // add a range of tables to dataset, same as adding columns to table
             ds.Tables.AddRange(new DataTable[] { dt3, dt4 });
 
             // Output the tables in the DataSet to the console.
-            Console.WriteLine("DataSet has {0} DataTables named: ",
-                ds.Tables.Count);
+            Console.WriteLine($"DataSet has {ds.Tables.Count} DataTables named");
             foreach (DataTable dt in ds.Tables)
-                Console.WriteLine("\t{0}", dt.TableName);
-
-            Console.WriteLine("\nPress any key to continue.");
-            Console.ReadKey();
-
+                Console.WriteLine($"\t{ dt.TableName}");
         }
     }
 }
